c5b483227aadbcae946f71d3e31ce10f8ac4218d
jonytalukdar <jonytalukdar30@gmail.com>
2022-01-16 06:42:05 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import Headers from '../src/components/Header/Header';
+import List from '../src/components/List/List';
+import Map from '../src/components/Map/Map';
+
+import { CssBaseline, Grid } from '@mui/material';
+
 const App = () => {
   return (
-    <div>
-      <h1>hello react</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Headers />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
