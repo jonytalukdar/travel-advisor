78191e0db6aa43e3634a59ed94263911218e8258
jonytalukdar <jonytalukdar30@gmail.com>
2022-01-18 19:11:21 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, InputBase, Box, Typography } from '@material-ui/core';
-import { Search } from '@material-ui/icons';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './HeaderStyles';
+import useStyles from './styles.js';
 
-const Header = () => {
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
 
   return (
@@ -17,19 +16,16 @@ const Header = () => {
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            Explore the new places
+            Explore new places
           </Typography>
-          {/* <Autocomplete> */}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <Search />
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-            <InputBase
-              placeholder="Search places"
-              classes={{ root: classes.inputRoot, input: classes.inputInput }}
-            />
-          </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,81 +1,47 @@
 import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-  Grid,
-  CircularProgress,
-} from '@material-ui/core';
-
-import useStyles from './ListStyles';
-
-const List = ({
-  places,
-  childClicked,
-  isLoading,
-  type,
-  setType,
-  rating,
-  setRating,
-}) => {
+const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
+  const [elRefs, setElRefs] = useState([]);
   const classes = useStyles();
 
-  //states
-  const [elRefs, setElRefs] = useState([]);
-
   useEffect(() => {
-    setElRefs((refs) =>
-      Array(places.length)
-        .fill()
-        .map((_, i) => refs[i] || createRef())
-    );
+    setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
   }, [places]);
 
   return (
     <div className={classes.container}>
-      <Typography variant="h5">
-        Restaurants, Hotels @ Attractions around you
-      </Typography>
-
+      <Typography variant="h4">Food & Dining around you</Typography>
       {isLoading ? (
         <div className={classes.loading}>
           <CircularProgress size="5rem" />
-          <h2>loading...</h2>
         </div>
       ) : (
         <>
-          {/* for select */}
           <FormControl className={classes.formControl}>
-            <InputLabel>Type</InputLabel>
-            <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
               <MenuItem value="restaurants">Restaurants</MenuItem>
               <MenuItem value="hotels">Hotels</MenuItem>
               <MenuItem value="attractions">Attractions</MenuItem>
             </Select>
           </FormControl>
-          {/* for ratings */}
           <FormControl className={classes.formControl}>
-            <InputLabel>Ratings</InputLabel>
-            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-              <MenuItem value="0">All</MenuItem>
-              <MenuItem value="3">Above 3</MenuItem>
-              <MenuItem value="4">Above 4</MenuItem>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
               <MenuItem value="4.5">Above 4.5</MenuItem>
             </Select>
           </FormControl>
-          <Grid className={classes.list} container spacing={3}>
+          <Grid container spacing={3} className={classes.list}>
             {places?.map((place, i) => (
-              <Grid ref={elRefs[i]} item key={i} xs={12}>
-                <PlaceDetails
-                  selected={Number(childClicked) === i}
-                  refProp={elRefs[i]}
-                  place={place}
-                />
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
               </Grid>
             ))}
           </Grid>
